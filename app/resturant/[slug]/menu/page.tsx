81414bad3fbe625057413f169ba611d4e3ede2c5
Aleksandar Atanasov <aleks930819@gmail.com>
2023-05-09 16:46:49 +0300

    code into layout to avoid duplicate code into pages

--- app/resturant/[slug]/menu/page.tsx
+++ app/resturant/[slug]/menu/page.tsx
@@ -5,18 +5,15 @@ import Menu from './components/Menu';
 
 const ResturantMenu = () => {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[100%] rounded p-3 shadow">
-            <RestaurantNavbar />
-            <Menu />
-          </div>
+    <>
+      <Header />
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        <div className="bg-white w-[100%] rounded p-3 shadow">
+          <RestaurantNavbar />
+          <Menu />
         </div>
-      </main>
-    </main>
+      </div>
+    </>
   );
 };
 
