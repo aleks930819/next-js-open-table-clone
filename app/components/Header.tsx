ccca43a9776a65ceaa803e9859eebe4c10c9f200
Aleksandar Atanasov <aleks930819@gmail.com>
2023-05-09 15:25:26 +0300

    added components

---
+++ app/components/Header.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Searchbar from './Searchbar';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const Header = () => {
+  const router = useRouter();
+  const [location, setLocation] = useState('');
+
+  return (
+    <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+      <div className="text-center mt-10">
+        <h1 className="text-white text-5xl font-bold mb-2">
+          Find your table for any occasion
+        </h1>
+        <Searchbar
+          location={location}
+          setLocation={setLocation}
+          router={router}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
