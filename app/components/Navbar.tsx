825582383c94f3bb6321a94f16c613b0ce617fad
Aleksandar Atanasov <aleks930819@gmail.com>
2023-05-10 16:15:49 +0300

    adde material ui to the project

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import LoginModal from './LoginModal';
 
 const Navbar = () => {
   return (
@@ -8,9 +9,8 @@ const Navbar = () => {
       </Link>
       <div>
         <div className="flex">
-          <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-            Sign in
-          </button>
+          <LoginModal isSignIn={true} />
+          <LoginModal isSignIn={false} />
           <button className="border p-1 px-4 rounded">Sign up</button>
         </div>
       </div>
---
+++ app/resturant/not-found.tsx
@@ -0,0 +1,21 @@
+'use client';
+import Image from 'next/image';
+import ErrorImage from '../../public/icons/error.png';
+const Error = ({ error }: { error: Error }) => {
+  return (
+    <div className="h-screen bg-gray-200 flex flex-col justify-center items-center">
+      <Image src={ErrorImage} alt="Error" width={500} height={500} />
+      <div className="bg-white px-9 py-14 shadow rounded">
+        <h3 className="text-3xl font-bold">Well, this is embarrassing</h3>
+        <p className="text-xl mt-4 text-red">
+          We are having trouble loading the page you are looking for.
+        </p>
+        {error && (
+          <p className="text-xl mt-4 text-red">Error: 404</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Error;
