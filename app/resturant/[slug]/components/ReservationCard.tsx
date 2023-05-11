260253fce8387c0e94d397057274daf1fd8f36ce
Aleksandar Atanasov <aleks930819@gmail.com>
2023-05-11 15:28:26 +0300

    added datepicker

--- app/resturant/[slug]/components/ReservationCard.tsx
+++ app/resturant/[slug]/components/ReservationCard.tsx
@@ -1,6 +1,16 @@
 'use client';
 
+import { partySize } from '../../../../data';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+
+
 const ReservationCard = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+
+  const handleChangeDate = (date: Date | null) => {
+    if (date) return setSelectedDate(date);
+  };
   return (
     <div className="fixed w-[15%] bg-white rounded p-3 shadow">
       <div className="text-center border-b pb-2 font-bold">
@@ -9,14 +19,21 @@ const ReservationCard = () => {
       <div className="my-3 flex flex-col">
         <label htmlFor="">Party size</label>
         <select name="" className="py-3 border-b font-light" id="">
-          <option value="">1 person</option>
-          <option value="">2 people</option>
+          {partySize.map(({ value, label }) => (
+            <option value={value}>{label}</option>
+          ))}
         </select>
       </div>
       <div className="flex justify-between">
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Date</label>
-          <input type="text" className="py-3 border-b font-light w-28" />
+          <DatePicker
+            className="py-3 border-b font-light text-reg w-28"
+            dateFormat={'MMMM d'}
+            wrapperClassName='w-[48%]'
+            selected={new Date()}
+            onChange={handleChangeDate}
+          />
         </div>
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Time</label>
